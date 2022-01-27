7fb9cecd76ac24725e51e0be5f53c858a5400855
shirohoo <shirohoo4102@gmail.com>
2022-01-27 22:59:41 +0900

    :sparkles: Movie 작성
    Signed-off-by: shirohoo <shirohoo4102@gmail.com>

--- 2.movie-reservation/src/main/java/io/github/shirohoo/moviereservation/domain/Movie.java
+++ 2.movie-reservation/src/main/java/io/github/shirohoo/moviereservation/domain/Movie.java
@@ -1,12 +1,34 @@
 package io.github.shirohoo.moviereservation.domain;
 
+import java.time.Duration;
+
 public class Movie {
+    private final String title;
+    private final Duration runningTime;
+    private final Money fee;
+    private DefaultDiscountPolicy discountPolicy;
+
+    private Movie(String title, Duration runningTime, Money fee, DefaultDiscountPolicy discountPolicy) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public static Movie of(String title, Duration runningTime, Money fee, DefaultDiscountPolicy discountPolicy) {
+        return new Movie(title, runningTime, fee, discountPolicy);
+    }
 
     public Money getFee() {
-        return null;
+        return fee;
     }
 
     public Money calculateMovieFee(Screening screening) {
-        return null;
+        Money discountAmount = discountPolicy.calculateDiscountAmount(screening);
+        return fee.minus(discountAmount);
+    }
+
+    public void setDiscountPolicy(DefaultDiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
     }
 }
