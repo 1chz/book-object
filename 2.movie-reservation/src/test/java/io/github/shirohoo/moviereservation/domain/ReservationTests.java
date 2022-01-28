6ca39c8fd895d2b1434623cf51010b995152cfc6
shirohoo <shirohoo4102@gmail.com>
2022-01-28 11:17:11 +0900

    :white_check_mark: ReservationTests 작성

---
+++ 2.movie-reservation/src/test/java/io/github/shirohoo/moviereservation/domain/ReservationTests.java
@@ -0,0 +1,19 @@
+package io.github.shirohoo.moviereservation.domain;
+
+import static io.github.shirohoo.moviereservation.fixture.DomainFixture.testMovie;
+import static io.github.shirohoo.moviereservation.fixture.DomainFixture.testScreening;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import org.junit.jupiter.api.Test;
+
+class ReservationTests {
+    @Test
+    void of() {
+        assertThatCode(() -> {
+            Customer customer = Customer.newCustomer("name");
+            Screening screening = testScreening(testMovie());
+            Money money = Money.won(10_000);
+            int audienceCount = 1;
+            Reservation.of(customer, screening, money, audienceCount);
+        }).doesNotThrowAnyException();
+    }
+}
