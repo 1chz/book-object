330364d3a745ca8599b75fd6b6164c4f3f44c6a4
shirohoo <shirohoo4102@gmail.com>
2022-01-28 11:07:06 +0900

    :sparkles: DomainFixture 작성

--- 2.movie-reservation/src/test/java/io/github/shirohoo/moviereservation/fixture/DomainFixture.java
+++ 2.movie-reservation/src/test/java/io/github/shirohoo/moviereservation/fixture/DomainFixture.java
@@ -4,16 +4,29 @@ import static java.time.LocalDateTime.now;
 import io.github.shirohoo.moviereservation.domain.DiscountPolicy;
 import io.github.shirohoo.moviereservation.domain.Money;
 import io.github.shirohoo.moviereservation.domain.Movie;
+import io.github.shirohoo.moviereservation.domain.NoneDefaultDiscountPolicy;
 import io.github.shirohoo.moviereservation.domain.Screening;
 import java.time.Duration;
-import java.time.LocalDateTime;
 
 public class DomainFixture {
-    public static Screening testScreening(DiscountPolicy discountPolicy) {
-        Movie movie = testMovie("titanic", Duration.ofMinutes(150), Money.won(9_900), discountPolicy);
-        int sequence = 1;
-        LocalDateTime when = now();
-        return Screening.of(movie, sequence, when);
+    public static Screening testScreening(Movie movie) {
+        return Screening.of(movie, 1, now());
+    }
+
+    public static Movie testMovie() {
+        return testMovie("titanic", Duration.ofMinutes(150), Money.won(9_900), new NoneDefaultDiscountPolicy());
+    }
+
+    public static Movie testMovie(Duration runningTime) {
+        return testMovie("titanic", runningTime, Money.won(9_900), new NoneDefaultDiscountPolicy());
+    }
+
+    public static Movie testMovie(Money fee) {
+        return testMovie("titanic", Duration.ofMinutes(150), fee, new NoneDefaultDiscountPolicy());
+    }
+
+    public static Movie testMovie(NoneDefaultDiscountPolicy discountPolicy) {
+        return testMovie("titanic", Duration.ofMinutes(150), Money.won(9_900), discountPolicy);
     }
 
     public static Movie testMovie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
